a15fa33ba546e3c9a58a88b1f14fed8b04ffca66
rahilazhar12 <158289635+rahilazhar12@users.noreply.github.com>
2024-05-16 17:18:58 +0500

    Add files via upload

--- client/src/Pages/Users/Userlogin.jsx
+++ client/src/Pages/Users/Userlogin.jsx
@@ -18,7 +18,7 @@ const UserLogin = () => {
         };
       
         try {
-          const response = await fetch('http://localhost:5000/api/v1/users/user-login', {
+          const response = await fetch(`${import.meta.env.VITE_API_URL}/api/v1/users/user-login`, {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
